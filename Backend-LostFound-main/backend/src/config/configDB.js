0155c7ba9b13696551bc3c3722cb6893b8b3cd75
Giovanniels <114963791+Giovanniels@users.noreply.github.com>
2024-07-26 00:45:28 -0400

    Update configDB.js

--- Backend-LostFound-main/backend/src/config/configDB.js
+++ Backend-LostFound-main/backend/src/config/configDB.js
@@ -1,40 +1,34 @@
 "use strict";
-// Importa el módulo 'mysql2' para crear la conexión a la base de datos
-const mysql = require("mysql2");
+// Importa el modulo 'mongoose' para crear la conexion a la base de datos
+const mongoose = require("mongoose");
 
-// Agregamos la configuración de las variables de entorno
+// Agregamos la configuracion de las variables de entorno
 const { configEnv } = require("./configEnv.js");
 const { handleError } = require("../utils/errorHandler");
 
 // Obtiene las variables de entorno
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = configEnv();
+const { DB_URL } = configEnv();
 
-// Crea la conexión a la base de datos
-const connection = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME
-});
+// Opciones de configuracion para la conexion a la base de datos
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 /**
  * @name setupDB
- * @description Función que crea la conexión a la base de datos
+ * @description Funcion que crea la conexion a la base de datos
  * @returns {Promise<void>}
  * @throws {Error}
  */
 async function setupDB() {
-  return new Promise((resolve, reject) => {
-    connection.connect((err) => {
-      if (err) {
-        handleError(err, "/configDB.js -> setupDB");
-        reject(err);
-      } else {
-        console.log("=> Conectado a la base de datos MySQL");
-        resolve();
-      }
-    });
-  });
+  try {
+    await mongoose.connect(DB_URL, options);
+    console.log("=> Conectado a la base de datos");
+  } catch (err) {
+    handleError(err, "/configDB.js -> setupDB");
+    throw new Error(err);
+  }
 }
 
-module.exports = { setupDB, connection };
+module.exports = { setupDB };
